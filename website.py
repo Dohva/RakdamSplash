358f6ac14b15f4df839e436d078084dfdbd48052
dohva <rallethekid@gmail.com>
2021-05-26 10:06:49 +0200

    Change headline

--- website.py
+++ website.py
@@ -3,10 +3,10 @@
 from splashgen import launch
 from splashgen.components import SplashSite, CTAButton
 
-site = SplashSite(title="Integrating CI via Github Actions",
+site = SplashSite(title="Rakdam Pipeline",
                   theme="dark")
 site.enable_splashgen_analytics = False
-site.headline = "Never run another console command again."
+site.headline = "Integrating CI via Github Actions"
 site.subtext = """
 Just build it in the cloud. It's easy. Automate everything and anything.
 This is python, but it'll work for nearly all languages you'll use.
